92a585a41c6116a749947547b36c3369c54b8d24
theb2ahir <baahirboukari4@gmail.com>
2025-07-18 16:05:14 +0000

    modification mineur

--- lib/Elements/mydrawer.dart
+++ lib/Elements/mydrawer.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:lindashopp/favoris.dart';
 import 'package:lindashopp/inquietude.dart';
 import 'package:lindashopp/nonlivre.dart';
 import 'package:url_launcher/url_launcher.dart';
